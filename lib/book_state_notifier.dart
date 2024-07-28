59a86adacb80b40e0f93dd19538523c4d042025e
hyeminililo <143622050+hyeminililo@users.noreply.github.com>
2024-07-28 22:27:10 +0900

    Feat : Create a file to manage widgets in a book

--- lib/book_state_notifier.dart
+++ lib/book_state_notifier.dart
@@ -1,13 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final booksProvider =
+final booksProvider = // 관리할 StateNotifier와 Notifier가 관리할 타입을 넣어줌
     StateNotifierProvider<BookStateNotifier, List<Book>>((ref) {
   return BookStateNotifier();
 });
 
+// List 형식의 책 상태를 관리할 것이라 제네릭을 통해 나타냄
 class BookStateNotifier extends StateNotifier<List<Book>> {
   BookStateNotifier() : super([]);
-
+// state = List<Book> -> 상태를 변경하는 것이라서 .add와 같은 메소드를 사용하지 않음
   void addBook(Book newBook) {
     state = [...state, newBook];
   }
---
+++ lib/book_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class BookWidget extends StatelessWidget {
+  final String title;
+  final String description;
+
+  const BookWidget({required this.title, required this.description, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(30)),
+      child: Row(
+        children: [
+          const Icon(Icons.book_rounded),
+          const SizedBox(
+            width: 16,
+          ),
+          RichText(
+              text: TextSpan(children: [
+            TextSpan(
+                text: "$title\n",
+                style: const TextStyle(
+                    color: Colors.red, fontWeight: FontWeight.bold)),
+            TextSpan(text: description, style: const TextStyle(fontSize: 12))
+          ]))
+        ],
+      ),
+    );
+  }
+}
